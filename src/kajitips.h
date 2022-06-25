8d9142fb7caac2292b792d8c2532b05495d3d8c5
Eric Branlund <ebranlund@fastmail.com>
2022-06-25 07:17:13 -0600

    Provide rough English translations for the jeweler and antique shop tips.  For Unix, change compile-wrap to preprocess kajitips.h so shift-JIS is converted to EUC.

--- src/kajitips.h
+++ src/kajitips.h
@@ -1,16 +1,16 @@
 static cptr kaji_tips_sh[5] =
 {
-	"現在持っているエッセンスの一覧を表示する。",
-	"アイテムからエッセンスを取り出す。エッセンスを取られたアイテムは消滅する。古道具屋は(武具・鉱石・一部の珍品)からエッセンスを抽出できる。",
-	"既にエッセンスが付加されたアイテムからエッセンスのみ消し去る。エッセンスは手に入らない。",
-	"アイテムにエッセンスを付加する。既にエッセンスが付加されたアイテムやアーティファクトには付加できない。古道具屋はすべての武具に対してエッセンスを付加できる。",
-	"武器や防具を強化したり、攻撃で傷つかないようにしたりする。エッセンスが付加されたアイテムやアーティファクトに対しても使用できる。",
+	_("現在持っているエッセンスの一覧を表示する。", "Display a list of the essences you currently have."),
+	_("アイテムからエッセンスを取り出す。エッセンスを取られたアイテムは消滅する。古道具屋は(武具・鉱石・一部の珍品)からエッセンスを抽出できる。", "Remove essence from an item.  Makes the item ordinary.  The antique shop can extract the essence from armor, ore, and some rare items"),
+	_("既にエッセンスが付加されたアイテムからエッセンスのみ消し去る。エッセンスは手に入らない。", "Erase added essence from an item.  The erased essence is not available for use."),
+	_("アイテムにエッセンスを付加する。既にエッセンスが付加されたアイテムやアーティファクトには付加できない。古道具屋はすべての武具に対してエッセンスを付加できる。", "Add essence to an item.  Essence can not be added to items or artifacts that already have added essence.  The antique shop can add essence to all weapons."),
+	_("武器や防具を強化したり、攻撃で傷つかないようにしたりする。エッセンスが付加されたアイテムやアーティファクトに対しても使用できる。", "Strengthen weapons and armor or prevent them from being hurt by attacks.  It can be used for items and artifacts with added essence."),
 };
 static cptr kaji_tips_jewel[5] =
 {
-	"現在持っているエッセンスの一覧を表示する。",
-	"アイテムからエッセンスを取り出す。エッセンスを取られたアイテムは消滅する。宝飾師は(装飾品・ランタン類・宝石・鉱石・一部の珍品)からエッセンスを抽出できる。",
-	"既にエッセンスが付加されたアイテムからエッセンスのみ消し去る。エッセンスは手に入らない。",
-	"アイテムにエッセンスを付加する。既にエッセンスが付加されたアイテムやアーティファクトには付加できない。宝飾師は(近接武器・指輪・アミュレット・ランタン類・リボン・頭防具)に対してエッセンスを付加できる。",
-	"武器や防具を強化したり、攻撃で傷つかないようにしたりする。エッセンスが付加されたアイテムやアーティファクトに対しても使用できる。",
+	_("現在持っているエッセンスの一覧を表示する。", "Display a list of the essences you currently have."),
+	_("アイテムからエッセンスを取り出す。エッセンスを取られたアイテムは消滅する。宝飾師は(装飾品・ランタン類・宝石・鉱石・一部の珍品)からエッセンスを抽出できる。", "Remove essence from an item.  Makes the item ordinary.  Jewelers can extract essence from adornments, lanterns, gems, ores, and some rare items."),
+	_("既にエッセンスが付加されたアイテムからエッセンスのみ消し去る。エッセンスは手に入らない。", "Erase added essence from an item.  The erased essence is not available for use."),
+	_("アイテムにエッセンスを付加する。既にエッセンスが付加されたアイテムやアーティファクトには付加できない。宝飾師は(近接武器・指輪・アミュレット・ランタン類・リボン・頭防具)に対してエッセンスを付加できる。", "Add essence to an item.  Essence can not be added to items or artifacts that already have added essence.  Jewelers can add essence to melee weapons, rings, amulets, lanterns, ribbons, and head armor."),
+	_("武器や防具を強化したり、攻撃で傷つかないようにしたりする。エッセンスが付加されたアイテムやアーティファクトに対しても使用できる。", "Strengthen weapons and armor or prevent them from being hurt by attacks.  It can be used for items and artifacts with added essence."),
 };
